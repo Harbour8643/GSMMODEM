26c2974c7fc1084e8f54815f2cb7ce4d7d92ae48
Harbour <935468819@qq.com>
2017-10-30 09:27:28 +0800

    添加项目文件。

---
+++ GSMTest/Common/AppConfig.cs
@@ -0,0 +1,42 @@
+﻿using Harbour.Utils;
+using System;
+
+namespace GSMTest.Common
+{
+    /// <summary>
+    /// WebApi全局IP
+    /// </summary>
+    public static class AppConfig
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string SocketIP
+        {
+            get
+            {
+                return ConfigHelper.GetConfigString("SocketIP"); 
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public static int SocketProt
+        {
+            get
+            {
+                return int.Parse(ConfigHelper.GetConfigString("SocketProt")); 
+            }
+        }
+        /// <summary>
+        /// COM端口
+        /// </summary>
+        public static String GtwId
+        {
+            get
+            {
+                return ConfigHelper.GetConfigString("GtwId"); 
+            }
+        }
+    }
+}
---
+++ GSMTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using GSMMODEM;
+using System;
+namespace GSMTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool opend = false;
+            string sResult = "";
+            GsmModem mGsmModem = null;
+            try
+            {
+                mGsmModem = new GsmModem();
+
+                mGsmModem.AutoDelMsg = true;
+                mGsmModem.ComPort = "COM3";
+                mGsmModem.BaudRate = 115200;
+                opend = mGsmModem.Open(out sResult);
+                if (opend)
+                {
+                    Console.WriteLine(string.Format("{0},{1} 打开成功", mGsmModem.ComPort, mGsmModem.BaudRate));
+                }
+                for (int i = 0; i < 20; i++)
+                    mGsmModem.SendMsg("10086", "CXYE");
+            }
+            catch (Exception ex)
+            {
+                if (mGsmModem.IsOpen)
+                {
+                    mGsmModem.Close();
+                }
+                Console.WriteLine(string.Format("{0},{1} 打开失败[{2}]", mGsmModem.ComPort, mGsmModem.BaudRate, sResult));
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                if (mGsmModem.IsOpen)
+                {
+                    mGsmModem.Close();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
